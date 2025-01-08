6313b48b1c0bf7c999f8fe35e58de296a9879729
Pawan Sutar <pawansutar@outlook.com>
2025-01-08 04:55:03 -0800

    Added the deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+const fs = require("fs/promises");
+
+async function main() {
+  const BankAccount = await hre.ethers.getContractFactory("BankAccount");
+  const bankAccount = await BankAccount.deploy();
+
+  await bankAccount.waitForDeployment();
+  await writeDeploymentInfo(bankAccount);
+}
+
+async function writeDeploymentInfo(contract) {
+  const data = {
+    contract: {
+      address: contract.address,
+      signerAddress: contract.signer,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
